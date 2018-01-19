2968abce4e398b2ac7a34d5aaf01adf55865076c
Jeroen van Hensbergen <j.v.hensbergen@hotmail.nl>
2018-01-19 21:29:22 +0000

    Functies in wezen af, returns nader te specificeren (hangt af van HTML). Errors nog te implementeren

--- Collectionneur/communities.py
+++ Collectionneur/communities.py
@@ -1,8 +1,9 @@
 class Communities():
 
     # Community aanmaken.
-    def create(self):
-
+    def create(self, community, user, summary):
+        db.execute("INSERT INTO community_page (username, description) VALUES(:username, :description)", username=user, description=summary)
+        db.execute("INSERT INTO community_users (communityname, username) VALUES(:communityname, :username)", communityname=community, username=user)
         return render_template(community.html)
 
     # Community verwijderen.
@@ -14,7 +15,7 @@ class Communities():
     # Lid worden.
     def join(self, name):
         db.execute("INSERT INTO community_users WHERE username=:username", username=name)
-        return redirect(url_for("overzicht"))
+        return redirect(url_for("community"))
 
     # Lid verwijderen.
     def remove(self, community, name):
