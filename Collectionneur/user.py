475ba43848749f12a17bdf306576feb177c50952
Lucas Oud <lucasoud@live.nl>
2018-01-22 14:27:15 +0000

    user class afgemaakt

--- Collectionneur/user.py
+++ Collectionneur/user.py
@@ -1,32 +1,43 @@
 from cs50 import SQL
-
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
 # configure CS50 Library to use SQLite database
 db = SQL("sqlite:///Collectionneur.db")
 
 ''' User class '''
 
 class User():
-    def __init__(self, username, password):
-        self.username = username
-
-    def registeruser():
+    def registeruser(username, hash1):
         # register user
         hash1 = pwd_context.hash(request.form.get("password"))
-        rows=db.execute("INSERT INTO users (username, hash) VALUES(:username, :password)", username=request.form.get("username"), password=hash1)
-        # let user log in
-        flash("Account succesfully created")
-        return redirect(url_for("login"))
+        rows=db.execute("INSERT INTO users (username, hash) VALUES(:username, :password)", username=username, password=hash1)
+        return True
 
-    def changepass():
+    def changepassword(hash1, id):
         # update password
-        db.execute("UPDATE users SET hash = :hash1 WHERE id= :id ",hash1 = hash1, id = session["user_id"])
+        db.execute("UPDATE users SET hash = :hash1 WHERE id= :id ",hash1 = hash1, id = id)
         # show index page
-        flash("Succesfully changed password")
-        return redirect(url_for("index"))
+        return True
 
-    def deleteaccount():
+    def deleteaccount(id):
         # delete user
-        db.execute("DELETE FROM users WHERE id= :id ", id = session["user_id"])
+        db.execute("DELETE FROM users WHERE id= :id ", id = id)
         # show index/log in page
-        flash("Succesfully deleted account")
-        return redirect(url_for("logout"))
+        return True
+
+    def userexist(username):
+        # return true if user exists
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=username)
+        return rows
+
+    def requestpassword(id):
+        rows = db.execute("SELECT hash FROM users WHERE id = :id", id=id)
+        return rows
+
+    '''
+    def mylists():
+        username = db.execute("SELECT username FROM user WHERE id=:id", id = session["user_id"])
+        lists = db.execute("SELECT list_name FROM lists WHERE owner=:username", username = username)
+        return lists
+    '''
