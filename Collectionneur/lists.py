7674137a69f99adeb950e2c114b9519c0d93c3dc
Gino Koppe <gino.koppe@live.nl>
2018-01-23 12:02:29 +0000

    lists.py aangepast

---
+++ Collectionneur/lists.py
@@ -0,0 +1,43 @@
+from cs50 import SQL
+from helpers import valid_id
+
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///Collectionneur.db")
+
+
+class Lists():
+
+
+    # lijst aanmaken
+    def create_list(owner, listname):
+        db.execute("INSERT INTO lists (owner, listname) VALUES(:owner, :listname)", owner=owner, listname=listname)
+        #return render_template("list.html")
+
+
+    # lijst verwijderen
+    def delete_list(listname):
+        db.execute("DELETE FROM lists WHERE id = :id", id=listname)
+        db.execute("DELETE FROM list_item WHERE list_id = :list_id", list_id=listname)
+        #return redirect(url_for("overzicht"))
+
+    # item aan lijst toevoegen
+    def add_item(name, film):
+        list_id = Lists.name_to_id(name)
+        lijstje = db.execute("SELECT * FROM list_item WHERE film = :film", film = film)
+        if film not in lijstje.values():
+            db.execute("INSERT INTO list_item (list_id, film_id) VALUES(:list_id, :film_id)", list_id=list_id, film_id=film)
+        else:
+             flash("film already in list")
+       # return redirect(url_for("list"))
+
+
+    # item verwijderen van lijst
+    def remove_item(list_id, film_id):
+        db.execute("DELETE FROM list_item WHERE list_id = :list_id AND film_id = :film_id", list_id=list_id, film_id=film_id)
+        #return redirect(url_for("list"))
+
+
+    #
+    def name_to_id(name):
+        return db.execute("SELECT id FROM lists WHERE owner = :name", name = name)
